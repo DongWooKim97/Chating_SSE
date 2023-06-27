7ca23b824a80504ae790d6dcee3b641bca41e6af
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-06-28 00:48:25 +0900

    feat: 기초 컨트롤러 생성

---
+++ src/main/java/com/ll/chating_sse/ChatController.java
@@ -0,0 +1,34 @@
+package com.ll.chating_sse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final List<ChatMessage> chatMessages = new ArrayList<>();
+
+    public record WriteMessageResponse(long id) {
+    }
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public ResultData writeMessage() {
+        ChatMessage msg = new ChatMessage("홍길동", "Hello World!");
+
+        chatMessages.add(msg);
+
+        return new ResultData(
+                "S-1",
+                "메세지가 작성되었습니다.",
+                new WriteMessageResponse(msg.getId())
+        );
+    }
+
+}
