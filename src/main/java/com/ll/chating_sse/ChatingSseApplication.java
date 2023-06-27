fb91e0afbf4653f95f7bdfbf84dc36e68b348b10
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-06-28 00:26:19 +0900

    first commit

---
+++ src/main/java/com/ll/chating_sse/ChatingSseApplication.java
@@ -0,0 +1,13 @@
+package com.ll.chating_sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatingSseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatingSseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/chating_sse/ChatingSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.chating_sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatingSseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
