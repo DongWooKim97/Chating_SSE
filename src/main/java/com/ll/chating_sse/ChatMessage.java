16b8b7b3a354b6f5a784520d2cc7f88cbe2ce135
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-06-28 00:48:45 +0900

    feat: 엔티티 성격을 가지는 채팅 메시지 클래스 생성

---
+++ src/main/java/com/ll/chating_sse/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.ll.chating_sse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@Getter
+public class ChatMessage {
+
+    private long id;
+    private LocalDateTime createDate;
+    private String authorName;
+    private String content;
+
+    public ChatMessage(String authorName, String content) {
+        this(ChatMessageIdGenerator.getNextId(), LocalDateTime.now(), authorName, content);
+    }
+
+}
+
+
+class ChatMessageIdGenerator {
+
+    private static long id = 0;
+
+    public static long getNextId() {
+        return ++id;
+    }
+}
